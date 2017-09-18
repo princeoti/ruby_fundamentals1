e1bfb20ce74bd41a12073b265bfae4a4004e3de1
PRINCEOTI <princeotti5@gmail.com>
2017-09-18 13:44:00 -0400

    finish some string examples

--- excercise1.rb
+++ excercise1.rb
@@ -2,4 +2,26 @@ puts  2 + 5
 puts 2
 puts 3
 puts 2 + 3
-puts 2 puts 3 puts 2 + 3
+
+
+ # irb assingment assingment done in terminal
+
+ # string assingment
+
+ puts 'Hello world'
+ puts "Hello world"
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+                  # double quotes
+
+# single interpolations
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+# Escape Sequences ( \" double quote, \\ single backslash, \a bell/alert, \b backspace, \r carriage return, \n newline, \s space, \t tab)
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"

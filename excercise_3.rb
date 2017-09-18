9bfc65dc6398155feb5ddcd02325bf9025776576
PRINCEOTI <princeotti5@gmail.com>
2017-09-18 16:08:06 -0400

    using puts and get to determing name, age and year of born

--- excercise_3.rb
+++ excercise_3.rb
@@ -1,1 +1,7 @@
 puts "What is your name?"
+user_name = gets
+puts "Hello, #{user_name}"
+puts "how old are you?"
+user_age = gets
+user_age = user_age.to_i
+puts "i believe you were born on #{2017-user_age}"
